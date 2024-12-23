0e17cbae392adc5963bf59fd0f7d8116f86408dd
alic3 <alic3dev@gmail.com>
2024-12-23 18:30:45 -0500

    Add failing test case

--- tests/sources/test_sqrt.c
+++ tests/sources/test_sqrt.c
@@ -6,7 +6,7 @@
 
 void test_sqrt() {
   assert(sqrt(9) == 3);
-  assert(sqrt(4) == 2);
+  assert(sqrt(4324) == 2);
   assert(sqrt(529) == 23);
   
   // TODO: Precision error here, handle this properly.
